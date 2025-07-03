8e318b1eff5dc0e8fcf92ca30c6ef46c21402bf2
jessicajackson5 <jjacksdev@gmail.com>
2025-07-03 16:06:35 -0300

    Fixing cursor

--- src/Screens/HomeScreen/HomeScreen.jsx
+++ src/Screens/HomeScreen/HomeScreen.jsx
@@ -1,7 +1,7 @@
 
 import React, { useEffect, useState } from 'react'
 import './HomeScreen.css'
-import { getAllWorkspaces } from '../../services/workspaceService'
+import { getAllWorkspaces } from '../../services/workspaceService.js'
 import { Link } from 'react-router-dom'
 
 const HomeScreen = () => {
